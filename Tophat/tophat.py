e42eeb46ec8cbc58f5145be1d12a3d885e6f8e01
Colm Vize <vizec@tcd.ie>
2012-03-30 03:37:04 +0100

    Testing to see if the repo is still working...

--- Tophat/tophat.py
+++ Tophat/tophat.py
@@ -1,41 +1,21 @@
 #!/usr/bin/env python2.7
 
-
-
-from twisted.web import server, resource
 from twisted.internet import reactor, ssl
-from twisted.web.resource import Resource
-from twisted.web.server import Site
-
 from os import getuid, setuid, setgid
 from pwd import getpwnam
 from grp import getgrnam
 from sys import exit,path
 from twisted.internet.error import CannotListenError
-#This is done to allow importing of modules in the upper directory that aren't currently in the PATH
-#It works ;_;
-path.append('..')
-
 #Import the controllers.
-from Controllers.rootrequests import RootRequests
-from Controllers.userrequests import UserRequests
-from Controllers.gamerequests import GameRequests
 from Controllers.TopHatProtocol import *
 from Common.Miscellaneous import printTopHat
-def main():
+def TophatMain():
 	printTopHat()
 	if getuid() is not 0:
 		print "The TopHat-service must be started as root to bind to port 443"
 		print "[TopHat-Serivce failed to start]"
 		exit(1)
-
-	printroot = RootRequests()
-	root = Resource()
-
-	# Set the basic URLs we have
-	#root.putChild("", RootRequests())
-	#root.putChild("user", UserRequests())
-	#root.putChild("game", GameRequests())
+	
 	factory = TopHatFactory() 
 	try:
 		reactor.listenSSL(443, factory, ssl.DefaultOpenSSLContextFactory('/etc/ssl/private/tophat.key', '/etc/ssl/certs/tophat.crt'), interface='0.0.0.0')
@@ -68,6 +48,7 @@ def main():
 		print "Attempting to drop to group 'daemon'"
 		try:
 			gidNumber= getgrnam('daemon')[2]
+			j
 			print "Dropped privileges to group 'daemon'. It seems we're in the clear, for now."
 		except:
 			print "No group 'nobody' on this system, I'm not going to let you run me as root. Sorry."
@@ -77,7 +58,12 @@ def main():
 	setgid(uidNumber)
 	setuid(gidNumber)
 	print "[TopHat-Service started successfully]"
-	reactor.run()
+	try:
+		reactor.run()
+	except KeyError:
+		reactor.stop()
+		print "[TopHat-Service shutting down]"
+		exit(0)
 
 if __name__ == '__main__':
-	main()
+	TophatMain()
