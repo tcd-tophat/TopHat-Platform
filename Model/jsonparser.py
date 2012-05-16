af337bd6c4a44df41569a0703c541d6e9495fb29
Kevin Bluett <kevin.bluett@gmail.com>
2012-05-16 18:12:29 +0100

    Closed #4 and fixed spelling mistake in the logger.

--- Model/jsonparser.py
+++ Model/jsonparser.py
@@ -1,7 +1,28 @@
+from types import StringType
 from twisted.web import server, resource
 from twisted.internet import reactor
+from Common.Log import LogFile
+from StringIO import StringIO
+import json
 
-# Simple Counter Main method to get us started with programming.
+# This class takes in JSON encoded Strings and returns them as objects
 class JsonParser(resource.Resource):
-    def __init__(resource):
-    	return
+
+    def __init__(self):
+    	self.log = LogFile()
+
+    # Get JSON as object, if fails, returns None.
+    def getObject(self, data):
+
+    	mapped = None
+
+    	try:
+    		assert type(data) is StringType
+
+    		toParse = StringIO(data)
+
+    		mapped = json.load(toParse)
+    	except AssertionError:
+    		self.log.write("Invalid type into the JSON Parser. Expecting StringType")
+
+    	return mapped
