cf6e45e99a07af410428ddf8ed435eac50795e80
Colm Vize <vizec@tcd.ie>
2012-05-21 18:35:17 +0100

    Adding some test JSON for app devs

--- View/jsonrenderer.py
+++ View/jsonrenderer.py
@@ -1,5 +1,3 @@
-from twisted.web import server, resource
-from twisted.internet import reactor
 
 # Simple Counter Main method to get us started with programming.
 class JsonRenderer(resource.Resource):
