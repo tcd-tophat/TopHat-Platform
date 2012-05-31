ae78e4f008e7637d70a96032f4d5fb15d1363865
Kevin Bluett <kevin.bluett@gmail.com>
2012-05-31 13:21:24 +0100

    Continuation of #20

--- Controllers/networking.py
+++ Controllers/networking.py
@@ -45,23 +45,27 @@ class TopHatThread(Thread):
 						if self.transport is None:
 								continue
 						client = TopHatClient(transport=self.transport)
+
+						# Provide a response class to share
+						response = HttpResponse()
+
 						HTTPParser(data[1], client)
 						request_value=-1
 						if str(client.state) == 'get':
 							from getrequest import getRequest
-							request_value = getRequest(client, data[1])
+							request_value = getRequest(client, response, data[1])
 
 						elif str(client.state) == 'put':
 							from putrequest import putRequest
-							request_value = putRequest(client, data[1], self.config.LogFile)
+							request_value = putRequest(client, response, data[1], self.config.LogFile)
 
 						elif str(client.state) == 'post':
 							from postrequest import postRequest
-							request_value = postRequest(client, data[1], self.config.LogFile)
+							request_value = postRequest(client, response, data[1], self.config.LogFile)
 
 						elif str(client.state) == 'delete':
 							from deleterequest import deleteRequest
-							request_value = deleteRequest(client,data[1])
+							request_value = deleteRequest(client, response, data[1])
 
 						elif str(client.state) == 'undef' or request_value is -1:
 							respondToClient(self.transport,'HTTP/1.1 400 Bad Request')
