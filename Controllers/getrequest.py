1a57c13fedbee2b48bbb7213c3c9aee4c54d17dd
Kevin Bluett <kevin.bluett@gmail.com>
2012-06-02 18:26:11 +0100

    Replaced old response parser and replaced it with new

--- Controllers/getrequest.py
+++ Controllers/getrequest.py
@@ -18,22 +18,19 @@ def getRequest(client, response, data):
 
 		Description:
 			Handles GET requests."""
-	data = data.rstrip()
-	data = data.split('\n') 
 
 	try:
-			header_http  = data[0].split('\n')[0]
-			request_path = header_http.split()[1]
 
-	except IndexError:
-			return -1
-	
-	path = request_path.split('/')[1]
+			http = HttpData(data, False)
 
-	if path == 'jsontest':
-			response.setCode(200)
-			response.setData ('{"glossary": {"title": "example glossary","GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML","SortAs": "SGML","GlossTerm": "Standard Generalized Markup Language","Acronym": "SGML","Abbrev": "ISO 8879:1986","GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso": ["GML", "XML"]},"GlossSee": "markup"}}}}}')
-			
+			if http.getDataPath() == "/jsontest":
+					response.setCode(200) # 501 = Unimplemented
+					response.setData ('{"glossary": {"title": "example glossary","GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML","SortAs": "SGML","GlossTerm": "Standard Generalized Markup Language","Acronym": "SGML","Abbrev": "ISO 8879:1986","GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso": ["GML", "XML"]},"GlossSee": "markup"}}}}}')
+
+			client.transport.write(response.constructResponse())
+	except:
+			# Respond with internal server error
+			response.setCode(500)
 
 	client.transport.write(response.constructResponse())
 	client.state.set_state('done')
