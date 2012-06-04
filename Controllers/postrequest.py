94d0fb8c192c8c0c639901585c939b36d916fe1b
Kevin Bluett <kevin.bluett@gmail.com>
2012-06-04 14:58:08 +0100

    Initial Implementation of Auth #15

--- Controllers/postrequest.py
+++ Controllers/postrequest.py
@@ -1,5 +1,7 @@
 from Model.jsonparser import JsonParser
 from Model.httpdata import HttpData
+from Mapper import usermapper as UM
+from Mapper import identityobject
 
 def postRequest (client, response, data, log):
 	"""Arguments:
@@ -24,12 +26,28 @@ def postRequest (client, response, data, log):
 			http = HttpData(data, True)
 
 			if http.getDataPath() == "/api/v1/apitokens":
-					response.setCode(200) # 501 = Unimplemented
-					response.setData ("{\"Feature coming soon!\":\"YAY\"}")
+
+					if http.getDataObject().has_key('username') and http.getDataObject().has_key('password'):
+
+						IO = identityobject.IdentityObject(None, user.User())
+						IO.field("email").eq(http.getDataObject()['username'])
+						usersSelect = UserMapper.selectIdentity(IO, 0, 1)
+
+						try:
+							user_object = usersSelect[0]
+
+							response.setCode(200) # 501 = Unimplemented
+							response.setData ("{\"Successfully retrieved user\":\"YAY\"}")
+
+						except KeyError:
+							response.setCode(400)
+
+
+
 			elif http.parseError():
 					# Respond with error 400 - Bad Request - if an parse error occurred inside the Http input responder
 					response.setCode(400)
-					
+
 			client.transport.write(response.constructResponse())
 	except:
 			# Respond with internal server error
