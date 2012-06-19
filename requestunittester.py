4bb16f549b6b64b143fc40f88d4fa147f04d18d6
Colm Vize <vizec@tcd.ie>
2012-06-19 12:43:24 +0100

    Added a unit test for the new WORKING protocol

--- requestunittester.py
+++ requestunittester.py
@@ -1,15 +1,31 @@
+#!/usr/bin/env python2.7
 from Model.textresponse import TextResponse
 
+from ssl import wrap_socket
+from socket import socket
+from sys import argv
+from struct import pack, unpack, error as HeaderFormatError
+def main():
+		ver=2
+		opcode=2
+		res=0
+		s = socket()
+		s.connect((argv[1],int(argv[2])))
+		s=wrap_socket(s)
+		data="Hiya: \"hi\""
+		uri="blackbird://test"
+		urilen=len(uri)
+		datalen=len(data)
+		header=pack("BBHHH", ver, opcode, res, datalen, urilen)
+		s.write(header)
+		s.write(uri)
+		s.write(data)
 
-try:
-	mod = __import__('Controllers.Requests.jsontest', fromlist=['Jsontest'])
-	klass = getattr(mod, 'Jsontest')
 
-	obj = klass(response)
-	
-	obj.get("json")
 
-	print response.constructResponse()
-except ImportError:
-	print "Class not found"
 
+
+
+if __name__ == '__main__':
+		main()
+		
