a87bb6ca0859a92d88fcf405f7e372195afec4e2
Kevin Bluett <kevin.bluett@gmail.com>
2012-06-26 12:48:28 +0100

    Changes :)

--- Networking/protocolhandler.py
+++ Networking/protocolhandler.py
@@ -5,3 +5,48 @@ class ProtocolHandler:
 
 	def __init__(self, protocol):
 		self.protocol = protocol
+
+
+	def load(self):
+		try:
+			mod = __import__('Controllers.Requests.'+resource, fromlist=[var])
+			klass = getattr(mod, var)
+
+			obj = klass(response)
+			
+			if type 	== 0:
+				obj.get(resource)
+			elif type 	== 1:
+				obj.post(resource, data)
+			elif type	== 2:
+				obj.put(resource, data)
+			elif type	== 3:
+				obj.delete(resource)
+
+		except:
+			response.setCode(404)
+
+	def getStatusCodes(self):
+
+		title = self.protocol.title()
+
+		mod = self.import_item('Networking.protocols.%s.statuscodes' % title)
+
+		klass = getattr(mod, "StatusCodes")
+
+		return klass
+
+	def import_item(self, name):
+	    """Import and return bar given the string foo.bar."""
+	    package = '.'.join(name.split('.')[0:-1])
+	    obj = name.split('.')[-1]
+
+	    if package:
+	        module = __import__(package,fromlist=[obj])
+	        try:
+	            pak = module.__dict__[obj]
+	        except KeyError:
+	            raise ImportError('No module named %s' % obj)
+	        return pak
+	    else:
+	        return __import__(obj)
--- moduleunittest.py
+++ moduleunittest.py
@@ -1,27 +1,15 @@
-from Model.textresponse import TextResponse
+from Networking.protocolhandler import ProtocolHandler
 import traceback, sys
 
-response = TextResponse()
+p = ProtocolHandler("twisted")
 
-resource = "jsontest"
 
-try:
-	var = resource.title()
+t = p.getStatusCodes()
 
-	try:
-		mod = __import__('Controllers.Requests.'+resource, fromlist=[var])
-		klass = getattr(mod, var)
+print t.OK
+print StatusCodes.OK
 
-		obj = klass(response)
-		
-		obj.get("json")
 
-	except:
-		response.setCode(404)
-		traceback.print_exc(file=sys.stdout)
-except:
-		# Respond with internal server error
-		response.setCode(500)
-		traceback.print_exc(file=sys.stdout)
+import pprint
 
-print response.constructStringResponse()
+pprint.pprint(t)
