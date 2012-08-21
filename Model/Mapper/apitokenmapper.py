19dc20895d8376afaef3dc57d91902cd3cfafd6f
Kevin Bluett <kevin.bluett@gmail.com>
2012-08-21 18:19:49 +0100

    Fixed an issue that caused the server to fail to start from the previous commit

--- Model/Mapper/apitokenmapper.py
+++ Model/Mapper/apitokenmapper.py
@@ -1,5 +1,4 @@
 from mapper import Mapp
-from Mapper.usermapper import UserMapper
 
 class ApitokenMapper(Mapp):
 
@@ -24,7 +23,8 @@ class ApitokenMapper(Mapp):
 	def _doCreateObject(self, data):
 		"""Builds the Apitoken object using the raw data provided from the database"""
 		from Model.apitoken import Apitoken
-
+		from Mapper.usermapper import UserMapper
+		
 		apitoken_ = Apitoken(data["id"])
 
 		apitoken_.setToken(data["key"])
